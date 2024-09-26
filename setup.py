77cf1d15f6dfd4ee55207c35dc6cc79900c838e2
sxydh <sxydhgg@gmail.com>
2024-09-26 21:48:37 +0800

    [UPDATE]

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="m_pyutil",
+    version="1.0.0",
+    author="sxydh",
+    author_email="sxydhgg@gmail.com",
+    description="TODO",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sxydh/m-pyutil",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
