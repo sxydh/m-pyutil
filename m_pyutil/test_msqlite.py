4e04b418b497af72d30129f41e2e6619c3247212
sxydh <sxydhgg@gmail.com>
2024-09-27 11:19:33 +0800

    [UPDATE]

--- m_pyutil/test_msqlite.py
+++ m_pyutil/test_msqlite.py
@@ -1,11 +1,14 @@
 import sqlite3
 from unittest import TestCase
 
-from m_pyutil.msqlite import get_conn
+from m_pyutil.msqlite import get_conn, create
 
 
 class Test(TestCase):
 
     def test_get_conn(self):
-        conn = get_conn()
-        self.assertIsInstance(conn, sqlite3.Connection)
+        with get_conn() as conn:
+            self.assertIsInstance(conn, sqlite3.Connection)
+
+    def test_create(self):
+        create(sql='create table t_test(id integer primary autoincreamentment)')
