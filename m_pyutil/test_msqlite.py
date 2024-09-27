b97af114a2cc55a27518f83be45aa8ec39b05a31
sxydh <sxydhgg@gmail.com>
2024-09-27 11:17:41 +0800

    [UPDATE]

--- m_pyutil/test_msqlite.py
+++ m_pyutil/test_msqlite.py
@@ -1,6 +1,11 @@
+import sqlite3
 from unittest import TestCase
+
+from m_pyutil.msqlite import get_conn
 
 
 class Test(TestCase):
+
     def test_get_conn(self):
-        self.fail()
+        conn = get_conn()
+        self.assertIsInstance(conn, sqlite3.Connection)
