9a77baa9644bc617ab568b71abd701668dc4c418
sxydh <sxydhgg@gmail.com>
2024-09-27 19:39:09 +0800

    [UPDATE]

--- m_pyutil/mtencent_map.py
+++ m_pyutil/mtencent_map.py
@@ -8,7 +8,16 @@ class TencentMap:
     def __init__(self, key: str):
         self.key = quote(key or '')
 
-    def geocoder(self, location: tuple):
-        lng = quote(location[0])
-        lat = quote(location[1])
-        return requests.get(f'https://apis.map.qq.com/ws/geocoder/v1?location={lat},{lng}&key={self.key}')
+    def geocoder(self, location: tuple = None, address: str = None):
+        url = 'https://apis.map.qq.com/ws/geocoder/v1'
+        if location:
+            lng = quote(location[0])
+            lat = quote(location[1])
+            location = quote(f'{lat},{lng}')
+            url = f'{url}?location={location}&key={self.key}'
+        elif address:
+            address = quote(address)
+            url = f'{url}?address={address}&key={self.key}'
+        else:
+            raise ValueError('location or address is required')
+        return requests.get(url)
