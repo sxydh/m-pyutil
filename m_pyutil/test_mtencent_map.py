85d6201410d373e4bb8263a1377241ec3f7dff03
sxydh <sxydhgg@gmail.com>
2024-09-27 19:41:27 +0800

    [UPDATE]

--- m_pyutil/test_mtencent_map.py
+++ m_pyutil/test_mtencent_map.py
@@ -8,6 +8,11 @@ class TestTencentMap(TestCase):
 
     def test_geocoder(self):
         tencent_map = TencentMap(os.environ.get('TENCENT_MAP_KEY'))
-        response = tencent_map.geocoder(('104.101589', '30.655520'))
+
+        response = tencent_map.geocoder(location=('104.101589', '30.655520'))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json().get('status'), 0)
+
+        response = tencent_map.geocoder(address='黔东南州凯里经济开发区金源西大道37号')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json().get('status'), 0)
