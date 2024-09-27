30bf6ad68b43c6ee58605aa3870f472c9d2df3b1
sxydh <sxydhgg@gmail.com>
2024-09-27 12:03:58 +0800

    [UPDATE]

--- m_pyutil/test_msqlite.py
+++ m_pyutil/test_msqlite.py
@@ -2,7 +2,7 @@ import sqlite3
 import uuid
 from unittest import TestCase
 
-from m_pyutil.msqlite import get_conn, create, drop, save
+from m_pyutil.msqlite import get_conn, create, drop, save, select
 
 
 class Test(TestCase):
@@ -16,12 +16,17 @@ class Test(TestCase):
         rowcount = create(sql=f'create table if not exists {self.table_name}(id integer primary key autoincrement, name text)')
         self.assertEqual(rowcount, -1)
 
+    def test_drop(self):
+        self.test_create()
+        rowcount = drop(sql=f'drop table {self.table_name}')
+        self.assertEqual(rowcount, -1)
+
     def test_save(self):
         self.test_create()
         rowcount = save(sql=f'insert into {self.table_name}(name) values(?)', params=[str(uuid.uuid4())])
         self.assertEqual(rowcount, 1)
 
-    def test_drop(self):
-        self.test_create()
-        rowcount = drop(sql=f'drop table {self.table_name}')
-        self.assertEqual(rowcount, -1)
+    def test_select(self):
+        self.test_save()
+        rows = select(sql=f'select * from {self.table_name}')
+        self.assertIs(len(rows) > 0, True)
