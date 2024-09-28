eb550a7435bedc8c3baf7479079a9421c9079ba1
sxydh <sxydhgg@gmail.com>
2024-09-28 16:54:21 +0800

    [UPDATE]

--- m_pyutil/mhttp.py
+++ m_pyutil/mhttp.py
@@ -21,7 +21,7 @@ class MyHTTPRequestHandler(CGIHTTPRequestHandler):
         if self.is_cors:
             self.send_header("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
             self.send_header("Access-Control-Allow-Origin", "*")
-            self.send_header("Access-Control-Allow-Headers", "Content-Type, Authorization")
+            self.send_header("Access-Control-Allow-Headers", "*")
         super().end_headers()
 
     def do_GET(self):
