c0f6bb0ca125025f271be323070aa62b36104623
sxydh <sxydhgg@gmail.com>
2024-09-28 09:50:27 +0800

    [UPDATE]

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="m_pyutil",
-    version="1.5.0",
+    version="1.5.1",
     author="sxydh",
     author_email="sxydhgg@gmail.com",
     description="NOTHING",
