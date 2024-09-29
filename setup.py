1d8dbb174a332d02d6a4706018bbf57aedb30640
sxydh <sxydhgg@gmail.com>
2024-09-29 18:42:31 +0800

    [UPDATE]

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="m_pyutil",
-    version="1.6.0",
+    version="1.6.1",
     author="sxydh",
     author_email="sxydhgg@gmail.com",
     description="NOTHING",
@@ -13,6 +13,9 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/sxydh/m-pyutil",
     packages=setuptools.find_packages(),
+    install_requires=[
+        'pymongo',
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
