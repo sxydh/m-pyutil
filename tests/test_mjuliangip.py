6e18c3bd6263a7f53d16d9fa1f32f294af8ac6c5
sxydh <sxydhgg@gmail.com>
2024-09-30 17:36:58 +0800

    [UPDATE]

---
+++ tests/test_mjuliangip.py
@@ -0,0 +1,12 @@
+import os
+from unittest import TestCase
+
+from m_pyutil.mjuliangip import JuLiangIP
+
+
+class Test(TestCase):
+
+    def test_dynamic_get_ips(self):
+        ju_liang_ip = JuLiangIP(os.environ.get('JULIANGIP_API_KEY'))
+        response = ju_liang_ip.dynamic_get_ips(trade_no=os.environ.get('JULIANGIP_TRADE_NO'))
+        self.assertEqual(response.status_code, 200)
