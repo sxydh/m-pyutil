fcb03b670482cf106d67baa88a0a735ec7c70589
sxydh <sxydhgg@gmail.com>
2024-09-30 18:36:07 +0800

    [UPDATE]

--- m_pyutil/mdate.py
+++ m_pyutil/mdate.py
@@ -1,16 +1,51 @@
 import datetime
+from enum import Enum
+
+
+class Format(Enum):
+    YYYY = '%Y'
+    YYYY_MM = '%Y-%m'
+    YYYY_MM_DD = '%Y-%m-%d'
+    YYYY_MM_DD_HH = '%Y-%m-%d %H'
+    YYYY_MM_DD_HH_MI = '%Y-%m-%d %H:%M'
+    YYYY_MM_DD_HH_MI_SS = '%Y-%m-%d %H:%M:%S'
+
+
+class DeltaType(Enum):
+    DAY = 3
+    HOUR = 4
+    MIN = 5
+    SEC = 6
 
 
 def nowt() -> str:
-    return datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    return datetime.datetime.now().strftime(Format.YYYY_MM_DD_HH_MI_SS.value)
 
 
 def nowd() -> str:
-    return datetime.datetime.now().strftime('%Y-%m-%d')
+    return datetime.datetime.now().strftime(Format.YYYY_MM_DD.value)
 
 
 def add_days(date_str: str,
-             date_format: str = '%Y-%m-%d',
              delta: int = 1) -> str:
-    d = datetime.datetime.strptime(date_str, date_format)
-    return (d + datetime.timedelta(days=delta)).strftime(date_format)
+    return add_times(time_str=date_str, delta=delta, delta_type=DeltaType.DAY)[:len(date_str)]
+
+
+def add_times(time_str: str,
+              delta: int = 1,
+              delta_type: DeltaType = DeltaType.SEC,
+              return_format: Format = Format.YYYY_MM_DD_HH_MI_SS) -> str:
+    full_time = '0000-00-00 00:00:00'
+    time_str = time_str + full_time[len(time_str):]
+    time = datetime.datetime.strptime(time_str, Format.YYYY_MM_DD_HH_MI_SS.value)
+    if delta_type == DeltaType.SEC:
+        time = time + datetime.timedelta(seconds=delta)
+    elif delta_type == DeltaType.MIN:
+        time = time + datetime.timedelta(minutes=delta)
+    elif delta_type == DeltaType.HOUR:
+        time = time + datetime.timedelta(hours=delta)
+    elif delta_type == DeltaType.DAY:
+        time = time + datetime.timedelta(days=delta)
+    else:
+        raise ValueError(f'delta_type={type(delta_type)} is not supported')
+    return time.strftime(return_format.value)
