2aabbf70bfe937aa0f5b567a6f37409d983ff00b
sxydh <sxydhgg@gmail.com>
2024-09-30 17:48:16 +0800

    [UPDATE]

--- m_pyutil/mjuliangip.py
+++ m_pyutil/mjuliangip.py
@@ -11,8 +11,11 @@ class ProxyProtocol(Enum):
 
 class DynamicIP:
 
-    def __init__(self, api_key: str):
+    def __init__(self,
+                 api_key: str,
+                 ip_time: int = 180):
         self.api_key = api_key
+        self.ip_time = ip_time
 
     def dynamic_get_ips(self,
                         trade_no: str,
