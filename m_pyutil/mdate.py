1d651eb101af711e1eb0fbf5cf83ab5ca53dfd80
sxydh <sxydhgg@gmail.com>
2024-09-30 17:59:32 +0800

    [UPDATE]

--- m_pyutil/mdate.py
+++ m_pyutil/mdate.py
@@ -1,6 +1,12 @@
 import datetime
 
 
-def add_days(date_str: str, date_format: str = '%Y-%m-%d', delta=1):
+def now() -> str:
+    return datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+
+def add_days(date_str: str,
+             date_format: str = '%Y-%m-%d',
+             delta: int = 1) -> str:
     d = datetime.datetime.strptime(date_str, date_format)
     return (d + datetime.timedelta(days=delta)).strftime(date_format)
