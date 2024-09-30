7745ceeb3d18a956dd87a011e7bedcec0f3775df
sxydh <sxydhgg@gmail.com>
2024-09-30 18:00:06 +0800

    [UPDATE]

--- m_pyutil/mdate.py
+++ m_pyutil/mdate.py
@@ -5,6 +5,10 @@ def nowt() -> str:
     return datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
 
+def nowd() -> str:
+    return datetime.datetime.now().strftime('%Y-%m-%d')
+
+
 def add_days(date_str: str,
              date_format: str = '%Y-%m-%d',
              delta: int = 1) -> str:
