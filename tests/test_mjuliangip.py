cf418aff6b410d18559e0f9fe291069b4e3820ac
sxydh <sxydhgg@gmail.com>
2024-09-30 19:10:56 +0800

    [UPDATE]

--- tests/test_mjuliangip.py
+++ tests/test_mjuliangip.py
@@ -8,6 +8,5 @@ class Test(TestCase):
 
     def test_dynamic_get_ips(self):
         ju_liang_ip = DynamicIP(os.environ.get('JULIANGIP_API_KEY'))
-        response = ju_liang_ip.dynamic_get_ips(trade_no=os.environ.get('JULIANGIP_TRADE_NO'))
-        self.assertEqual(response.status_code, 200)
-        print(response.json())
+        ips = ju_liang_ip.get_ips(trade_no=os.environ.get('JULIANGIP_TRADE_NO'))
+        print(ips)
