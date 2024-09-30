5f6150c24b97d971e11472a01791dd7939fdc3e9
sxydh <sxydhgg@gmail.com>
2024-09-30 19:16:56 +0800

    [UPDATE]

--- tests/test_mjuliangip.py
+++ tests/test_mjuliangip.py
@@ -7,6 +7,6 @@ from m_pyutil.mjuliangip import DynamicIP
 class Test(TestCase):
 
     def test_dynamic_get_ips(self):
-        ju_liang_ip = DynamicIP(os.environ.get('JULIANGIP_API_KEY'))
-        ips = ju_liang_ip.get_ips(trade_no=os.environ.get('JULIANGIP_TRADE_NO'))
+        dynamic_ip = DynamicIP(os.environ.get('JULIANGIP_API_KEY'))
+        ips = dynamic_ip.get_ips(trade_no=os.environ.get('JULIANGIP_TRADE_NO'))
         print(ips)
