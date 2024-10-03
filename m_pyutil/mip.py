ba7cc67d5a13e8d093f6b4a2af5b89bad0a1e207
sxydh <sxydhgg@gmail.com>
2024-10-04 00:15:30 +0800

    [UPDATE]

--- m_pyutil/mip.py
+++ m_pyutil/mip.py
@@ -1,11 +1,28 @@
+import ipaddress
 import socket
+from typing import Any
 
 import psutil
 
 
-def get_ip(nic_name: str = 'WLAN'):
-    for name, addresses in psutil.net_if_addrs().items():
+def get_ip(nic_name: str = 'WLAN') -> str:
+    return get_address_info(nic_name).address
+
+
+def get_netmask(nic_name: str = 'WLAN') -> str:
+    return get_address_info(nic_name).netmask
+
+
+def get_broadcast(nic_name: str = 'WLAN') -> str:
+    ip = get_ip(nic_name)
+    netmask = get_netmask(nic_name)
+    network = ipaddress.ip_network(f'{ip}/{netmask}', strict=False)
+    return network.broadcast_address.exploded
+
+
+def get_address_info(nic_name) -> Any:
+    for name, named_tuples in psutil.net_if_addrs().items():
         if name == nic_name:
-            for address in addresses:
-                if address.family == socket.AF_INET:
-                    return address.address
+            for named_tuple in named_tuples:
+                if named_tuple.family == socket.AF_INET:
+                    return named_tuple
--- tests/test_mip.py
+++ tests/test_mip.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from m_pyutil.mip import get_ip
+from m_pyutil.mip import get_ip, get_netmask, get_broadcast
 
 
 class Test(TestCase):
@@ -9,3 +9,13 @@ class Test(TestCase):
         ip = get_ip()
         regexp = r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
         self.assertRegex(ip, regexp)
+
+    def test_get_netmask(self):
+        netmask = get_netmask()
+        regexp = r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
+        self.assertRegex(netmask, regexp)
+
+    def test_get_broadcast(self):
+        broadcast = get_broadcast()
+        regexp = r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
+        self.assertRegex(broadcast, regexp)
