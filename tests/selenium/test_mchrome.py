f4fe31e451174b7d7cfd702e9a8005ee050c5823
sxydh <sxydhgg@gmail.com>
2024-10-05 16:16:57 +0800

    [UPDATE]

--- tests/selenium/test_mchrome.py
+++ tests/selenium/test_mchrome.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from m_pyutil.selenium.mchrome import ChromeCli
+from m_pyutil.selenium.mchrome import ChromeCli, UcChromeCli
 
 
 class TestChromeCli(TestCase):
@@ -9,5 +9,15 @@ class TestChromeCli(TestCase):
         chrome_cli = ChromeCli()
         chrome_cli.get('https://www.baidu.com')
         nav = chrome_cli.query_element_d('id', 's-top-left')
+        nav_text = nav.text
+        self.assertIsInstance(nav_text, str)
+
+
+class TestUcChromeCli(TestCase):
+
+    def test_get(self):
+        uc_chrome_cli = UcChromeCli()
+        uc_chrome_cli.get('https://www.baidu.com')
+        nav = uc_chrome_cli.query_element_d('id', 's-top-left')
         nav_text = nav.text
         self.assertIsInstance(nav_text, str)
